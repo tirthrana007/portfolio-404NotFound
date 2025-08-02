d6d3a70b7dc0296b8aabf274ede34ac13d22e67c
tirthrana007 <ranatirth7@gmail.com>
2025-08-02 14:39:44 +0530

    Update Navbar.jsx

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             href="/"
             className="text-xl font-bold transition-colors text-neutral-400 hover:text-white"
           >
-            Ali
+            404NotFound
           </a>
           <button
             onClick={() => setIsOpen(!isOpen)}
