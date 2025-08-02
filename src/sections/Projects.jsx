7b2ee12429d0c1f8eee81f523d16e9d870c5cedd
tirthrana007 <ranatirth7@gmail.com>
2025-08-02 15:01:12 +0530

    Update Projects.jsx

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
       onMouseMove={handleMouseMove}
       className="relative c-space section-spacing"
     >
-      <h2 className="text-heading">My Selected Projects</h2>
+      <h2 className="text-heading">Our Selected Projects</h2>
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full" />
       {myProjects.map((project) => (
         <Project key={project.id} {...project} setPreview={setPreview} />
